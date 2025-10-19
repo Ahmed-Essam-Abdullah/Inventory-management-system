582c03135c6aa43e70d1f0eccf542075532f983d
Ahmedehab098 <ahmedehabali1234aaaaa@gmail.com>
2025-10-20 02:44:16 +0300

    finish the cutomer product class

--- src/CustomerProduct.java
+++ src/CustomerProduct.java
@@ -7,6 +7,50 @@
  *
  * @author ADMIN
  */
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CustomerProduct {
-    
+
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+    private final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+public String lineRepresentation() {
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public String getSearchKey() {
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter);
+    }
 }
